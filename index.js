cb530035f89686537b0812d4a1198337c89e6d6d
Karim <krmgafarov@gmail.com>
2023-06-24 10:20:25 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,71 @@
+console.log("I ______ to work every day");
+console.log("Select your answer :");
+console.log("a : went ");
+console.log("b : will go");
+console.log("c : go ");
+console.log("d : are going");
+var result = '';
+var i = prompt("Enter answer : ");
+if (i != "c"){
+    console.log("Wrong answer!")
+}
+else if(i === "c"){
+    console.log("Correct!")
+}
+else {
+    console.log("Error")
+}
+
+console.log("She _______ me coffee every morning");
+console.log("Select your answer :");
+console.log("a : are bringig ");
+console.log("b : bringed");
+console.log("c : will bring ");
+console.log("d : brings");
+var result = '';
+var w = prompt("Enter answer : ");
+if (w != "d"){
+    console.log("Wrong answer!")
+}
+else if(w === "d"){
+    console.log("Correct!")
+}
+else {
+    console.log("Error")
+}
+
+console.log("They usually _____ tennis every weekend ");
+console.log("Select your answer :");
+console.log("a : play ");
+console.log("b : are playing");
+console.log("c : will play ");
+console.log("d : plays");
+var result = '';
+var q = prompt("Enter answer : ");
+if (q != "a"){
+    console.log("Wrong answer!")
+}
+else if(q === "a"){
+    console.log("Correct!")
+}
+else {
+    console.log("Error")
+}
+
+console.log("It _____ in winter ");
+console.log("Select your answer :");
+console.log("a : snow ");
+console.log("b : are snowing");
+console.log("c : snows ");
+console.log("d : will snowing");
+var result = '';
+var z = prompt("Enter answer : ");
+if (z != "c"){
+    console.log("Wrong answer!")
+}
+else if(z === "c"){
+    console.log("Correct!")
+}
+else {
+    console.log("Error")
+}
